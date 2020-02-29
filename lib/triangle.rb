cbfd0d5e7e68281293c79d6413787c2c9952fbaa
Derric Fragosa <fderric@gmail.com>
2020-02-29 06:00:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,21 +1,41 @@
+require 'pry'
 class Triangle
-  attr_accessor :x, :y, :z
-  
-  def initialize(x, y, z)
-    @x = x 
-    @y = y 
-    @z = z 
-  end
-  
-  def equilateral
+    attr_accessor :angle_x, :angle_y, :angle_z, :kind
     
-  end
+    def initialize(angle_x, angle_y, angle_z)
+      @angle_x = angle_x
+      @angle_y = angle_y
+      @angle_z = angle_z
+    end
   
-  def isosceles
-    
-  end 
-  
-  def scalene
-    
-  end
+    def kind
+     if (angle_x * angle_y * angle_z) == 0 || (angle_x + angle_y) <= angle_z || (angle_y + angle_z) <= angle_x || (angle_z + angle_x) <= angle_y 
+          raise TriangleError
+     elsif angle_x < 0 || angle_y < 0 || angle_z < 0
+          raise TriangleError
+          elsif angle_x == nil || angle_y == nil || angle_z == nil
+          raise TriangleError
+      
+        
+     elsif angle_x == angle_y && angle_x == angle_z
+        self.kind = :equilateral
+        
+     elsif angle_x == angle_y || angle_x  == angle_z || angle_y == angle_z
+        self.kind = :isosceles
+        
+     else
+        self.kind = :scalene
+     end 
+    end
 end
+
+  class TriangleError < StandardError
+    def message
+      puts "Not A Triangle"
+    end
+  end
+  
+#Straight angle (180-degrees)
+#Acute angle (less than 90-degrees)
+#Right angle (90-degrees)
+#Obtuse angle (More than 90-degrees but less than 180 degrees)
