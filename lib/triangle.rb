5f1fc1164db5569d291bbaed554b91c62220077a
Derric Fragosa <fderric@gmail.com>
2020-02-29 05:58:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z
+  
+  def initialize(x, y, z)
+    @x = x 
+    @y = y 
+    @z = z 
+  end
+  
+  def equilateral
+    
+  end
+  
+  def isosceles
+    
+  end 
+  
+  def scalene
+    
+  end
 end
